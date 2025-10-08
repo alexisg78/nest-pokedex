eb7b964a90fd1a6a54e6775c6dbd6cdb1e8e7e30
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-10-07 23:22:56 -0300

    Paginación de Pokemons

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,6 +5,7 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto.js';
 
 
 
@@ -34,8 +35,20 @@ export class PokemonService {
     
   }
 
-  findAll() {
+  findAll( paginationDto : PaginationDto ) {
+    
+    const { limit= 10, offset= 0 } = paginationDto;
+    
     return this.pokemonModel.find()
+      .limit( limit )
+      .skip( offset )
+      .sort({
+        no: 1
+      })
+      .select('-__v')
+      
+
+
   }
 
   async findOne(term: string) {
